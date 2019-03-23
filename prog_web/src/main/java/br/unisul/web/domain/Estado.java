a248ed4f63695efce14c138870ac370f409189bf
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2019-03-22 21:44:50 -0300

    Adiciona hashcode e equals nas classes

--- prog_web/src/main/java/br/unisul/web/domain/Estado.java
+++ prog_web/src/main/java/br/unisul/web/domain/Estado.java
@@ -1,4 +1,5 @@
 package br.unisul.web.domain;
+
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -37,5 +38,29 @@ public class Estado implements Serializable {
 		this.nome = nome;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
